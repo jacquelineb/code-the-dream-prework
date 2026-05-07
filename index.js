25d6517a192de10b1401ee02f83e090760512908
jacquelineb <jacquelinesotheremail@gmail.com>
2026-05-06 21:15:34 -0700

    Fetch and display artworks

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-function createImageLink(id) {
-  return `https://www.artic.edu/iiif/2/${id}/full/843,/0/default.jpg`;
+const IIFE_IMAGE_SIZE = {
+  SMALL: 200,
+  MEDIUM: 400,
+  LARGE: 600,
+  FULL: 843,
+};
+
+function createImageLink(imageId, size = IIFE_IMAGE_SIZE.SMALL) {
+  return `https://www.artic.edu/iiif/2/${imageId}/full/${size},/0/default.jpg`;
 }
 
 async function getArtworks() {
-  const url = 'https://api.artic.edu/api/v1/artworks?limit=12';
+  const FIELDS_PARAMETER = `fields=${['id', 'title', 'api_link', 'thumbnail', 'artist_display', 'image_id'].join(',')}`;
+  const url = `https://api.artic.edu/api/v1/artworks?page=1&limit=12&${FIELDS_PARAMETER}`;
+  console.log(url);
   try {
     const response = await fetch(url);
-    const result = await response.json();
-    console.log(result);
-    const artworks = result.data.map((artworkData) => {
-      return {
-        api_link: artworkData.api_link,
-        thumbnail: artworkData.thumbnail,
-      };
-    });
+    const artworks = (await response.json()).data;
     console.log(artworks);
     return artworks;
   } catch (error) {
@@ -25,9 +27,25 @@ async function getArtworks() {
 
 async function displayArtworks() {
   const artworks = await getArtworks();
-  const img = document.createElement('img');
-  img.src = artworks[10].thumbnail.lqip;
-  img.width = artworks[10].thumbnail.width / 5;
-  img.height = artworks[10].thumbnail.height / 5;
+  // const artworkContainer = document.createElement('div');
+  const artworkSection = document.getElementById('artworks');
+  const artworkGrid = document.createElement('div');
+  artworkGrid.setAttribute('class', 'art_grid');
+
+  artworks.forEach((artwork) => {
+    const img = document.createElement('img');
+    // img.src = artwork.thumbnail.lqip;
+    img.src = createImageLink(artwork.image_id);
+    img.width = '300';
+
+    const imgContainer = document.createElement('div');
+    imgContainer.appendChild(img);
+    artworkGrid.appendChild(imgContainer);
+  });
+  artworkSection.appendChild(artworkGrid);
+  // const img = document.createElement('img');
+  // img.src = artworks[10].thumbnail.lqip;
+  // img.width = artworks[10].thumbnail.width / 5;
+  // img.height = artworks[10].thumbnail.height / 5;
 }
 displayArtworks();
