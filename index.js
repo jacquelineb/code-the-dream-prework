5e967891bcc10833e782b3d689cf8957715aef47
jacquelineb <jacquelinesotheremail@gmail.com>
2026-05-11 19:15:11 -0700

    Add media queries

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ const IIFE_IMAGE_SIZE = {
   FULL: 843,
 };
 
-function createImageLink(imageId, size = IIFE_IMAGE_SIZE.SMALL) {
+function createImageLink(imageId, size = IIFE_IMAGE_SIZE.MEDIUM) {
   return `https://www.artic.edu/iiif/2/${imageId}/full/${size},/0/default.jpg`;
 }
 
 async function getArtworks() {
   const FIELDS_PARAMETER = `fields=${['id', 'title', 'api_link', 'thumbnail', 'artist_display', 'image_id'].join(',')}`;
-  const url = `https://api.artic.edu/api/v1/artworks?page=1&limit=12&${FIELDS_PARAMETER}`;
+  const url = `https://api.artic.edu/api/v1/artworks?page=1&${FIELDS_PARAMETER}`;
   console.log(url);
   try {
     const response = await fetch(url);
@@ -47,7 +47,7 @@ async function displayArtworks() {
   // const artworkContainer = document.createElement('div');
   const artworkSection = document.getElementById('artworks');
   const artworkGrid = document.createElement('div');
-  artworkGrid.setAttribute('class', 'art_grid');
+  artworkGrid.setAttribute('class', 'artwork_grid');
 
   artworks.forEach((artwork) => {
     const img = document.createElement('img');
@@ -55,9 +55,14 @@ async function displayArtworks() {
     img.src = createImageLink(artwork.image_id);
     // img.width = '300';
 
-    const imgContainer = document.createElement('div');
-    imgContainer.appendChild(img);
-    artworkGrid.appendChild(imgContainer);
+    const artworkContainer = document.createElement('div');
+    artworkContainer.appendChild(img);
+    // const p = document.createElement('p');
+    // p.textContent = artwork.title;
+    // artworkContainer.appendChild(p);
+
+    artworkContainer.setAttribute('class', 'art_container');
+    artworkGrid.appendChild(artworkContainer);
   });
   artworkSection.appendChild(artworkGrid);
   // const img = document.createElement('img');
@@ -65,6 +70,8 @@ async function displayArtworks() {
   // img.width = artworks[10].thumbnail.width / 5;
   // img.height = artworks[10].thumbnail.height / 5;
 }
+
+function displayPagination() {}
 
 displayNavbar();
 displayArtworks();
