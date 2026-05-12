a9ef7a4da09e91c7bda719f4bf216c3949fd96c6
jacquelineb <jacquelinesotheremail@gmail.com>
2026-05-12 15:34:11 -0700

    Add title and artist of artwork

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ function createImageLink(imageId, size = IIFE_IMAGE_SIZE.MEDIUM) {
 }
 
 async function getArtworks() {
-  const FIELDS_PARAMETER = `fields=${['id', 'title', 'api_link', 'thumbnail', 'artist_display', 'image_id'].join(',')}`;
-  const url = `https://api.artic.edu/api/v1/artworks?page=1&${FIELDS_PARAMETER}`;
+  const FIELDS_PARAMETER = `fields=${['id', 'title', 'api_link', 'thumbnail', 'artist_title', 'image_id'].join(',')}`;
+  const url = `https://api.artic.edu/api/v1/artworks?page=1&limit=24&${FIELDS_PARAMETER}`;
   console.log(url);
   try {
     const response = await fetch(url);
@@ -52,6 +52,16 @@ async function displayArtworks() {
 
     const artworkContainer = document.createElement('div');
     artworkContainer.appendChild(img);
+
+    const title = document.createElement('div');
+    title.textContent = artwork.title;
+    artworkContainer.appendChild(title);
+    title.setAttribute('class', 'title');
+
+    const artist = document.createElement('div');
+    artist.textContent = artwork.artist_title;
+    artworkContainer.appendChild(artist);
+    artist.setAttribute('class', 'artist');
 
     artworkContainer.setAttribute('class', 'art_container');
     artworkGrid.appendChild(artworkContainer);
