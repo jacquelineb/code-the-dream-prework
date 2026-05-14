489a938405a3c51628530775599dedbad10f6336
jacquelineb <jacquelinesotheremail@gmail.com>
2026-05-14 00:16:48 -0700

    Add button to show product description

--- shop.js
+++ shop.js
@@ -3,7 +3,6 @@ import { createPagination } from './global.js';
 async function fetchShop(page) {
   const FIELDS_PARAMETER = `fields=${['description', 'image_url', 'title', 'max_current_price'].join(',')}`;
   const url = `https://api.artic.edu/api/v1/products?page=${page}&limit=12&${FIELDS_PARAMETER}`;
-  // const url = 'https://api.artic.edu/api/v1/products?limit=12';
   try {
     const response = await fetch(url);
     const result = await response.json();
@@ -12,15 +11,55 @@ async function fetchShop(page) {
       products: result.data,
       pagination: result.pagination,
     };
-    // return result.data;
   } catch (error) {
     console.error(error);
   }
 }
 
+function createModal() {
+  const modal = document.createElement('div');
+  modal.setAttribute('id', 'modal');
+
+  const modalContent = document.createElement('div');
+  modalContent.setAttribute('id', 'modal_content');
+  modal.appendChild(modalContent);
+
+  const closeButton = document.createElement('button');
+  closeButton.textContent = 'Close';
+  closeButton.onclick = () => {
+    modal.style.display = 'none';
+  };
+  modal.appendChild(closeButton);
+
+  modal.style.display = 'none';
+  document.getElementById('products').appendChild(modal);
+}
+
+function displayProductModal(name, description) {
+  console.log('DISPLAY THIS');
+  // const modalContent = document.createElement('div');
+  // modalContent.setAttribute('class', 'modal');
+
+  const productName = document.createElement('div');
+  productName.textContent = name;
+  // modalContent.appendChild(productName);
+
+  const productDescription = document.createElement('div');
+  productDescription.innerHTML = description;
+  // modalContent.appendChild(productDescription);
+
+  const modalContent = document.getElementById('modal_content');
+  // modal.innerHTML = '';
+  // modal.appendChild(productName);
+  // modal.appendChild(productDescription);
+  modalContent.replaceChildren(productName, productDescription);
+
+  document.getElementById('modal').style.display = 'block';
+}
+
 function displayProducts(products) {
   products.forEach((product) => {
-    console.log(product);
+    // console.log(product);
     const img = document.createElement('img');
     img.src = product.image_url;
     const productImgContainer = document.createElement('div');
@@ -33,17 +72,28 @@ function displayProducts(products) {
 
     const productInfo = document.createElement('div');
     const name = document.createElement('div');
+    name.setAttribute('class', 'product_name');
     name.textContent = product.title;
     productInfo.appendChild(name);
 
     const price = document.createElement('div');
     price.textContent = `$${product.max_current_price.toFixed(2)}`;
+    price.setAttribute('class', 'price');
     productInfo.appendChild(price);
 
     const description = document.createElement('div');
     description.innerHTML = product.description;
     description.setAttribute('class', 'description');
     productInfo.appendChild(description);
+
+    const viewDescriptionBtn = document.createElement('button');
+    viewDescriptionBtn.textContent = 'View description';
+    viewDescriptionBtn.onclick = () => {
+      // console.log(product.title);
+      // console.log(product.description);
+      displayProductModal(product.title, product.description);
+    };
+    productInfo.appendChild(viewDescriptionBtn);
 
     productContainer.appendChild(productInfo);
     document.getElementById('products').appendChild(productContainer);
@@ -52,28 +102,29 @@ function displayProducts(products) {
 
 (async () => {
   const params = new URLSearchParams(document.location.search);
-  console.log(params);
   let pageNumber = params.get('page');
-  console.log(typeof pageNumber);
   if (pageNumber === null) {
     pageNumber = 1;
   } else if (isNaN(Number(pageNumber)) || Number(pageNumber) <= 0) {
     window.location.href = window.location.href.split('?')[0];
   }
 
-  console.log(pageNumber);
+  // console.log(pageNumber);
   const shop = await fetchShop(pageNumber);
   if (shop === undefined) {
     const error = document.createElement('div');
     error.textContent = 'Unable to fetch shop data at this time.';
     document.getElementById('products').appendChild(error);
   } else {
+    createModal();
     const { products, pagination } = shop;
     displayProducts(products);
     // console.log(shop);
     // displayPagination(pagination.total_pages, document.getElementById('products'));
     const paginationElement = createPagination(pagination.total_pages, 'shop.html');
     document.getElementById('products').appendChild(paginationElement);
+
+    // displayProductModal('test', 'test description');
   }
   // */
 })();
